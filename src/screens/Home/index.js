4d183b89944ea7b779a944587e15ce9261123682
Tiago Almeida <tiago.almeida@decode.pt>
2021-05-27 14:53:46 +0100

    Barra de pesquisa

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,15 @@
 
 import React, { useState, useEffect } from 'react';
-import { Text, View, FlatList, Button, StyleSheet, Image } from 'react-native';
+import { Text, View, FlatList, Button, StyleSheet, Image, TextInput } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import Api from '../../services/Api'
 
 
 export default () => {
     const [products, setProducts] = useState([]);
+    const [filterdProducts, setFilterProducts] = useState([]);
 
+    const [search, setSearch] = useState('');
     useEffect(() => {
         getProducts();
     }, []);
@@ -18,10 +21,25 @@ export default () => {
         console.log(data)
         setProducts(data);
 
-    }
-    const viewDetail = async () => {
 
     }
+    const searchFilter = (text) => {
+        if (text) {
+            const newProducts = products.filter((item) => {
+                const itemData = item.nome ? item.nome.toUpperCase() : ''.toUpperCase()
+                const textData = text.toUpperCase()
+                return itemData.indexOf(textData) > -1
+
+            })
+            setProducts(newProducts)
+            setSearch(text)
+        } else {
+            setProducts(products)
+            setSearch(text)
+
+        }
+    }
+
 
 
 
@@ -36,27 +54,48 @@ export default () => {
 
 
     return (
-        <FlatList
-            data={products}
-            renderItem={({ item }) => (
-                <View style={styles.product}>
-                    <View style={styles.imageContainer}>
-                        <Image style={styles.image} source={{ uri: item.imagem }} />
-                    </View>
-                    <View style={styles.details}>
-                        <Text style={styles.title}>{item.nome}</Text>
-                        <Text style={styles.price}>{item.preco}€</Text>
+        <SafeAreaView>
+            <TextInput
+                placeholder="Procurar"
+                placeholderTextColor='#888'
+                style={styles.input}
+                value={search}
+                onChangeText={(text) => searchFilter(text)}
+            />
+            <FlatList
+                data={products}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                    <View style={styles.product}>
+                        <View style={styles.imageContainer}>
+                            <Image style={styles.image} source={{ uri: item.imagem }} />
+                        </View>
+                        <View style={styles.details}>
+                            <Text style={styles.title}>{item.nome}</Text>
+                            <Text style={styles.price}>{item.preco}€</Text>
+                        </View>
+
+                        <View style={styles.actions}>
+                            <Button color='#2a2e4a' title="Adicionar Carrinho" />
+                        </View>
                     </View>
 
-                    <View style={styles.actions}>
-                        <Button color='#2a2e4a' title="Adicionar Carrinho" />
-                    </View>
-                </View>
 
-            )} />
+                )}
+            />
+        </SafeAreaView>
     )
 }
 const styles = StyleSheet.create({
+    input: {
+        backgroundColor: '#2a2e4a',
+        height: 50,
+        fontSize: 18,
+        padding: 10,
+        color: 'white',
+        borderBottomWidth: 0.5,
+
+    },
     product: {
         shadowColor: 'black',
         shadowOpacity: 0.26,
